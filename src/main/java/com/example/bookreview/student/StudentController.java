02e3253ef8d05f1e9086559ad7a30bbf54cd245a
manq2010 <manq2010@gmail.com>
2023-02-02 09:06:58 +0200

    Add update 'put' students

--- src/main/java/com/example/bookreview/student/StudentController.java
+++ src/main/java/com/example/bookreview/student/StudentController.java
@@ -38,4 +38,12 @@ public class StudentController {
             @PathVariable("studentId") Long studentId) {
         studentService.deleteStudent(studentId);
     }
+
+    @PutMapping(path = "/{studentId}")
+    public void updateStudent(
+            @PathVariable Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
+    }
 }
